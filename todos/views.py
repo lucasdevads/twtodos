1fb8ca541a4532c6beb4f486f8278e3703ff3522
lucasdevads <lucasadsmorais@gmail.com>
2025-03-18 07:41:22 -0300

    viwes

--- todos/views.py
+++ todos/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Óla Mundo!, Seja bem Vindo Lucas")
+    return render(request, "todos/home.html")
 
 
