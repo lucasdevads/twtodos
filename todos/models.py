e686189d67bc7c7185ecfc8c1494cfb8c1dc0245
lucasdevads <lucasadsmorais@gmail.com>
2025-03-18 07:50:24 -0300

    class em Models

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-class
+class Todo(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False)
+    create_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+    deadline = models.DateTimeField(null=False, blank=False)
+    finished_at = models.DateTimeField(null=True, )
