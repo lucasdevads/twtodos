49d07045cf05a174924f3915cf10d46a658d9b05
lucasdevads <lucasadsmorais@gmail.com>
2025-03-26 10:40:28 -0300

    black formatação

--- todos/views.py
+++ todos/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home(request):
     return render(request, "todos/home.html")
 
 
 def page(request):
-    return render(request,)
+    return render(
+        request,
+    )
